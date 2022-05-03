85cfc7b3c772993e355d4d8363179540be18f2e5
StephenIvan93 <stephensheppard93@gmail.com>
2022-05-03 09:24:52 +0000

    Setup structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+function runGame() {
+
+}
+
+function checkanswer() {
+
+}
+
+function calculateCorrectanswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionquestion() {
+
+}
+
+function displaysubtractQuestion() {
+
+}
+
+function displayMultiplyquestion() {
+
+}
+
+
